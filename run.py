fdc9f4e27bc09841495073d24e3ed443614d5294
Carina <carrie.poell@gmail.com>
2019-07-04 17:42:25 +0200

    Add index.html

--- run.py
+++ run.py
@@ -1,12 +1,14 @@
 import os
-from flask import Flask, redirect
+from datetime import datetime
+from flask import Flask, redirect, render_template
 
 app = Flask(__name__)
 messages = []
 
 def add_messages(username, message):
     """Add messages to the 'messages' list"""
-    messages.append("{}: {}".format(username, message))
+    now = datetime.now().strftime("%H:%M:%S")
+    messages.append("({}) {}: {}".format(now, username, message))
 
 def get_all_messages():
     """Get all of the messages and separate them a 'br'"""
@@ -15,7 +17,7 @@ def get_all_messages():
 @app.route('/')
 def index():
     """ Main page with instructions """
-    return "To send a message use /USERNAME/MESSAGE"
+    return render_template("index.html")
 
 @app.route("/<username>")
 def user(username):
