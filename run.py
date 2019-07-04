44721d4c8a2eacca438269a97cb440327ca2afb3
Carina <carrie.poell@gmail.com>
2019-07-04 17:52:36 +0200

    Add session cookies to project.

--- run.py
+++ run.py
@@ -1,8 +1,9 @@
 import os
 from datetime import datetime
-from flask import Flask, redirect, render_template
+from flask import Flask, redirect, render_template, request, session
 
 app = Flask(__name__)
+app.secret_key = "randomstring123"
 messages = []
 
 def add_messages(username, message):
@@ -14,9 +15,15 @@ def get_all_messages():
     """Get all of the messages and separate them a 'br'"""
     return "<br>".join(messages)
 
-@app.route('/')
+@app.route('/', methods = ["GET", "POST"])
 def index():
     """ Main page with instructions """
+    if request.method == "POST":
+        session["username"] = request.form["username"]
+    
+    if "username" in session:
+        return redirect(session["username"])
+
     return render_template("index.html")
 
 @app.route("/<username>")
