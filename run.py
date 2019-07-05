e9aa0fd3276bcf1136e36d7c27218937ada39734
Carina <carrie.poell@gmail.com>
2019-07-05 14:43:51 +0200

    add textbox

--- run.py
+++ run.py
@@ -24,8 +24,15 @@ def index():
 
     return render_template("index.html")
 
-@app.route("/<username>")
+@app.route("/<username>", methods = ["GET", "POST"])
 def user(username):
+    if request.method == "POST":
+        username = session["username"]
+        message = request.form["message"]
+        add_messages(username, message)
+
+        return redirect(session["username"])
+
     return render_template("chat.html", username = username, chat_messages = messages)
 
 
